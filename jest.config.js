05558eaf42ffe97f4b79b1344422a8724aedef8e
matheusperuzzo <matheusperuzzodev@gmail.com>
2023-06-06 08:30:00 -0400

    ensure LoadUserByEmailRepository return an user if user is found

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   collectCoverage: false,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig']
 }
