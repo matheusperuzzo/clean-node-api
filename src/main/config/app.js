dc9c341fd67794b131d9176ed584e4150c94ad03
matheusperuzzo <matheusperuzzodev@gmail.com>
2023-06-14 12:31:42 -0400

    add routes file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const setupApp = require('./setup')
 const app = express()
+const setupRoutes = require('./routes')
 
 setupApp(app)
+setupRoutes(app)
 
 module.exports = app
